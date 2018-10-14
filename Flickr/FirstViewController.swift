d571f17248657d87e67568c97a8069632b479d54
Dmitry <bulletproof024@gmail.com>
2018-10-14 17:49:52 +0700

    Project creation

---
+++ Flickr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Flickr
+//
+//  Created by Дмитрий Вашлаев on 14.10.18.
+//  Copyright © 2018 Дмитрий Вашлаев. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'Flickr' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'ReactiveCocoa', '~> 7.0'
+end
